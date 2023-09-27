65bf60c43a87d87b8ea758c2e4ef2c5f237b91a9
Adreeezz <127537483+Adreeezz@users.noreply.github.com>
2023-09-27 21:12:38 +0000

    test_remove_student_from_desired_grade pass

--- test_grade_school.c
+++ test_grade_school.c
@@ -371,7 +371,7 @@ static void test_student_not_added_to_same_grade_more_than_once(void)
 
 static void test_remove_student_from_desired_grade(void)
 {
-   TEST_IGNORE();
+   //TEST_IGNORE();
    uint8_t desired_grade = 2;
    student_t expected[] = {
       { 2, "Blair" }, 
